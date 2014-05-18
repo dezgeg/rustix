2064e4ed4b389499ce34e6f904dbb3a3e256d207
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-05-18 03:00:15 +0300

    Add 'yes'.

---
+++ src/yes.rs
@@ -0,0 +1,11 @@
+use std::os;
+use std::str::StrVector;
+
+fn main() {
+    let args = os::args();
+    let s = if args.len() == 1 { ~"y" } else { args.slice_from(1).connect(" ") };
+
+    loop {
+        println!("{}", s);
+    }
+}
